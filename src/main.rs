df84eafc72b021a7bb9ee156064c49a30d6842b5
牛浩然 <hrniu@isoftstone.com>
2024-01-16 11:09:48 +0800

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,115 @@
-fn main() {
-    println!("Hello, world!");
+
+extern crate dotenv;
+
+use dotenv::dotenv;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, middleware};
+use sqlx::mysql::MySqlPool;
+use std::env;
+
+mod models;
+
+use models::{Article, NewArticle, UpdateArticle};
+
+async fn get_all_article(pool: web::Data<MySqlPool>) -> impl Responder {
+    let result = sqlx::query_as::<_, Article>("SELECT * FROM article")
+        .fetch_all(pool.get_ref())
+        .await;
+
+    match result {
+        Ok(Article) => HttpResponse::Ok().json(Article),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+
+async fn get_article_by_id(
+    id: web::Path<i32>,
+    pool: web::Data<MySqlPool>
+) -> impl Responder {
+    let article_id = id.into_inner();
+
+    match sqlx::query_as::<_, Article>("SELECT * FROM Article WHERE id = ?")
+        .bind(article_id)
+        .fetch_one(pool.get_ref())
+        .await
+    {
+        Ok(article) => HttpResponse::Ok().json(article),
+        Err(_) => HttpResponse::NotFound().into(),
+    }
+}
+
+async fn add_article(
+    new_article: web::Json<NewArticle>,
+    pool: web::Data<MySqlPool>
+) -> impl Responder {
+    let article = new_article.into_inner();
+
+    match sqlx::query("INSERT INTO Article (title, content) VALUES (?, ?)")
+        .bind(&article.title)
+        .bind(&article.content)
+        .execute(pool.get_ref())
+        .await
+    {
+        Ok(_) => HttpResponse::Created().finish(), // 直接使用 .finish()
+        Err(_) => HttpResponse::InternalServerError().finish(), // 同理
+    }
+}
+
+async fn update_article(
+    id: web::Path<i32>,
+    article_data: web::Json<UpdateArticle>,
+    pool: web::Data<MySqlPool>
+) -> impl Responder {
+    let article_id = id.into_inner();
+    let article = article_data.into_inner();
+
+    match sqlx::query("UPDATE Article SET title = ?, content = ? WHERE id = ?")
+        .bind(&article.title)
+        .bind(&article.content)
+        .bind(article_id)
+        .execute(pool.get_ref())
+        .await
+    {
+        Ok(_) => HttpResponse::Created().finish(), // 直接使用 .finish()
+        Err(_) => HttpResponse::InternalServerError().finish(), // 同理
+    }
+}
+
+async fn delete_article(
+    id: web::Path<i32>,
+    pool: web::Data<MySqlPool>
+) -> impl Responder {
+    let article_id = id.into_inner();
+
+    match sqlx::query("DELETE FROM Article WHERE id = ?")
+        .bind(article_id)
+        .execute(pool.get_ref())
+        .await
+    {
+        Ok(_) => HttpResponse::Created().finish(), // 直接使用 .finish()
+        Err(_) => HttpResponse::InternalServerError().finish(), // 同理
+    }
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = MySqlPool::connect(&database_url).await.expect("Failed to create pool.");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .route("/Article", web::get().to(get_all_article))
+            .route("/Article/{id}", web::get().to(get_article_by_id))
+            .route("/Article", web::post().to(add_article))
+            .route("/Article/{id}", web::put().to(update_article))
+            .route("/Article/{id}", web::delete().to(delete_article))
+            .wrap(middleware::Logger::default())
+    })
+        .bind("127.0.0.1:9090")?
+        .run()
+        .await
 }
---
+++ src/models.rs
@@ -0,0 +1,30 @@
+use serde::{Serialize, Deserialize};
+use sqlx::FromRow;
+use sqlx::types::chrono::NaiveDateTime;
+
+#[derive(Serialize, Deserialize, FromRow)]
+pub struct Article {
+    pub id: i32,
+    pub title: Option<String>,
+    pub content: Option<String>,
+    pub view_num: Option<i32>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+    pub deleted_at: Option<NaiveDateTime>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct NewArticle {
+    pub title: String,
+    pub content: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UpdateArticle {
+    pub title: Option<String>,
+    pub content: Option<String>,
+    pub view_num: Option<i32>,
+    // 不包括 created_at，因为它通常在创建时自动生成并且之后不变
+    pub updated_at: Option<NaiveDateTime>,
+    pub deleted_at: Option<NaiveDateTime>,
+}
